7551f01369cfa3bd495a468fff83b1878b98abf1
Scai <59282365+alexevladgabriel@users.noreply.github.com>
2024-09-01 01:10:03 +0300

    cleaning session

--- test/readme_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule ReadmeTest do
-  use ExUnit.Case, async: true
-
-  test "versions match" do
-    readme = Path.join(__DIR__, "../README.md") |> File.read!()
-    version = Mix.Project.config()[:version]
-    assert readme =~ ~s({:elixir_a2s, "~> #{version}"})
-  end
-end
